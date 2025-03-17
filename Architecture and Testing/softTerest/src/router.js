0a1cf90d06f210e0c45c1ae56ad5f01ef2dca743
Vladislav Andonov <wladoandonov@gmail.com>
2025-03-17 12:44:32 +0200

    Added all solved exercises from JS-Applications

---
+++ Architecture and Testing/softTerest/src/router.js
@@ -0,0 +1,50 @@
+export function initialize(links) {
+  const main = document.querySelector("main");
+  const nav = document.querySelector("nav");
+  nav.addEventListener("click", onNavigate);
+
+  const context = {
+    showSection,
+    goTo,
+    updateNav
+  };
+
+  return context;
+
+  //Render the page
+  function showSection(section) {
+    main.replaceChildren(section);
+  }
+
+  //Click handler
+  function onNavigate(event) {
+    let target = event.target;
+    if (target.tagName == "IMG") {
+      target = target.parentElement;
+    }
+    if (target.tagName == "A") {
+      event.preventDefault();
+      const url = new URL(target.href);
+      goTo(url.pathname);
+    }
+  }
+
+  //Pass context to the handler
+  function goTo(name, ...params) {
+    const handler = links[name];
+    if (typeof handler == "function") {
+      handler(context, ...params);
+    }
+  }
+
+  function updateNav() {
+    const user = localStorage.getItem("user");
+    if (user) {
+      nav.querySelectorAll(".user").forEach((e) => (e.style.display = "block"));
+      nav.querySelectorAll(".guest").forEach((e) => (e.style.display = "none"));
+    } else {
+      nav.querySelectorAll(".user").forEach((e) => (e.style.display = "none"));
+      nav.querySelectorAll(".guest").forEach((e) => (e.style.display = "block"));
+    }
+  }
+}
